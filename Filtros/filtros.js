473ec5ed82f232062dc2364d67ee1d34362c29fd
HMrles <hmorales@gmx.com>
2015-02-12 07:34:11 -0600

    Uso de Filtros con AngularJS

---
+++ Filtros/filtros.js
@@ -0,0 +1,28 @@
+function controladorFiltros($scope){
+	$scope.empleados = [
+		{
+			nombre:'Linus', apellidoPaterno: 'Benedectic', apellidoMaterno: 'Torvals', fecha: new Date(),
+			salario: 12000, telefono:'2271110101', bono: 10.1416
+		},
+		{
+			nombre:'Alan', apellidoPaterno: 'Turing', apellidoMaterno: '', fecha: new Date(),
+			salario: 18000, telefono:'2271114548', bono: 18.1416
+		},
+		{
+			nombre:'Augusta ', apellidoPaterno: 'Ada', apellidoMaterno: 'King', fecha: new Date(),
+			salario: 20000, telefono:'2254110101', bono: 8.1416
+		},
+		{
+			nombre:'Charles ', apellidoPaterno: 'Babbage', apellidoMaterno: '', fecha: new Date(),
+			salario: 12000, telefono:'2271110101', bono: 10.1416
+		},
+		{
+			nombre:'Dennis', apellidoPaterno: 'Ritchie', apellidoMaterno: 'MacAlistair ', fecha: new Date(),
+			salario: 9000, telefono:'2271110464', bono: 0.1416
+		},
+		{
+			nombre:'Marissa ', apellidoPaterno: 'Ann', apellidoMaterno: 'Mayer', fecha: new Date(),
+			salario: 13000, telefono:'227110305', bono: 3.1416
+		},
+	]
+}
